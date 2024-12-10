ee757a8e30e6a18eb84ce4fda733648273fedd39
Sumon Ahmed <csesumon8@gmail.com>
2024-12-10 21:02:54 +0600

    add code for list

--- dataType.dart
+++ dataType.dart
@@ -1,13 +1,38 @@
 void main() {
-  final int a = 20;
-  const int b = 15;
-  const name = "Aag";
+  Person p1 = Person("Devid", "male", 35);
+  p1.showData();
 
-  print("The sum is ${a + b}");
-  list();
+  TestList l1 = TestList();
+  l1.showList();
 }
 
-void list() {
-  var list1 = [1, 2, 3, 4, 5];
-  print(list1);
+class Person {
+  String? name, sex;
+  int? age;
+
+  //Constructor
+  Person(String name, sex, int age) {
+    this.name = name;
+    this.sex = sex;
+    this.age = age;
+  }
+
+  void showData() {
+    print(" $name" " $sex" " $age");
+  }
+}
+
+class TestList {
+  void showList() {
+    var myList = [1, 2, 3, 4, 5];
+    print(myList);
+    myList.add(7);
+    print(myList);
+    myList.insert(3, 400);
+    print(myList);
+    var mixedlist = [17, "Sumon", "SEU"];
+    print(mixedlist);
+    mixedlist.removeAt(1);
+    print(mixedlist);
+  }
 }
