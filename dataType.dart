288f4f2ad4a2c9f8257dbb72dcb678f7873f51bc
Sumon Ahmed <csesumon8@gmail.com>
2024-12-10 17:19:27 +0600

    add list function

--- dataType.dart
+++ dataType.dart
@@ -2,10 +2,12 @@ void main() {
   final int a = 20;
   const int b = 15;
   const name = "Aag";
-  
-  print(name);
-  print(a + b);
-  print("change name");
-  print(a + b);
-  print("change name");
+
+  print("The sum is ${a + b}");
+  list();
+}
+
+void list() {
+  var list1 = [1, 2, 3, 4, 5];
+  print(list1);
 }
